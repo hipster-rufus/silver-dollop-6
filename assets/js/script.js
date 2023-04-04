609ce7b1ef10010dbeac0e9b407fc6bf3f9b73e2
Kali Lott <kalilott@Kalis-MacBook-Pro.local>
2023-04-03 23:56:32 -0600

    Revise JS, add more styling '

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,30 @@
-var apiKey = '4af0193721309dff41bbabaded87109e';
-var callOpenWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+apiKey;
-var callGeocoding = 'https://api.openweathermap.org/geo/1.0/direct?q='+cityName+'&limit=none&appid='+apiKey;
-var searchInput = document.getElementById('city-search');
-var searchBtn = document.getElementById('search-btn');
+const apiKey = '4af0193721309dff41bbabaded87109e';
+const callOpenWeather = 'https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid='+apiKey;
+const callGeocoding = 'https://api.openweathermap.org/geo/1.0/direct?q='+cityName+'&limit=none&appid='+apiKey;
+const searchInput = document.getElementById('city-search');
+const searchBtn = document.getElementById('search-btn');
+const currentForecast = document.getElementById('5day-forecast');
 var cityName;
-var currentForecast = document.getElementById('5day-forecast');
 
 searchBtn.addEventListener('click', () => {
-    cityName = searchInput.value;
-
+    cityName = '';
+    currentCity.innerHTML = '';
+    currentDate.innerHTML = '';
+    currentConditions.innerHTML = '';
+    currentTemp.textContent = '';
+    currentWind.textContent = '';
+    currentHumidity.textContent = '';
+    getCoordinates();
     getForecast();
+    displayWeather();
 });
+
+searchInput.addEventListener('keydown', function(event) {
+    if (event.key === 13) {
+        event.preventDefault();
+        searchBtn.click();
+    }
+})
 
 function getCoordinates() {
     fetch(callGeocoding)
@@ -36,12 +50,12 @@ function getForecast() {
     })
 }
 
-var currentCity = document.getElementById('city-name');
-var currentDate = document.getElementById('main-date');
-var currentConditions = document.getElementById('main-conditions');
-var currentTemp = document.getElementById('main-temp');
-var currentWind = document.getElementById('main-wind');
-var currentHumidity = document.getElementById('main-humidity');
+var currentCity = document.$('#city-name');
+var currentDate = document.$('#main-date');
+var currentConditions = document.$('#main-conditions');
+var currentTemp = document.$('#main-temp');
+var currentWind = document.$('#main-wind');
+var currentHumidity = document.$('#main-humidity');
 
 function displayWeather() {
     fetch(callOpenWeather)
@@ -50,11 +64,18 @@ function displayWeather() {
     })
     .then(function (data) {
         console.log(data);
-        currentCity[i] = data.city.name;
-        currentDate[i] = data.list.dt;
-        currentConditions[i] = data.list.weather.icon;
-        currentTemp[i] = data.list.main.temp;
-        currentWind[i] = data.list.wind.speed;
-        currentHumidity[i] = data.list.main.humidity;
+        currentCity.innerHTML = data.city.name;
+        currentDate.innerHTML = data.list.dt;
+        currentConditions.setAttribute("src", "") = data.list.weather.icon;
+        currentTemp.textContent = 'Temp: '+data.list.main.temp+' °F';
+        currentWind.textContent = 'Wind: '+data.list.wind.speed+' MPH';
+        currentHumidity.textContent = 'Humidity: '+data.list.main.humidity+' %';
     })
+};
+
+
+
+function saveSearch() {
+    prevSearchEl = document.createElement("li");
+    prevSearchEl.textContent = prevSearch;
 }
